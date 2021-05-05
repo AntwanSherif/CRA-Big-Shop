cb530691d25d9192f815cf3cadaa3ca353545d9b
AntwanSherif <antwansherif@gmail.com>
2021-05-05 22:35:43 +0200

    :sparkles: Fetch products from a fake store API

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import {
   ChakraProvider,
   Box,
   Text,
-  Link,
   VStack,
-  Code,
   Grid,
   theme,
 } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 import { ColorModeSwitcher } from './components/ColorModeSwitcher';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(res => res.json())
+      .then(json => setProducts(json));
+  }, []);
+
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
@@ -18,17 +25,16 @@ function App() {
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
             <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
+              Welcome to <Text fontSize="3xl">Everything React!</Text>
             </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
+
+            {products.map(({ id, title, image, price }) => (
+              <div key={id}>
+                <h4>{title}</h4>
+                <div>{price}</div>
+                <img src={image} width={100} height={100} alt={title} />
+              </div>
+            ))}
           </VStack>
         </Grid>
       </Box>
