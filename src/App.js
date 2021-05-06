bb8e4b4720f95787c03bfaefe80430a05eb619c9
AntwanSherif <antwansherif@gmail.com>
2021-05-06 23:36:47 +0200

    :children_crossing: Handle loading state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import { ChakraProvider, Box, Heading, Grid, theme } from '@chakra-ui/react';
+import {
+  ChakraProvider,
+  Box,
+  Heading,
+  Grid,
+  Spinner,
+  theme,
+} from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import { ColorModeSwitcher } from './components/ColorModeSwitcher';
 import { ProductCard } from './components/ProductCard';
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
+
     fetch('https://fakestoreapi.com/products')
       .then(res => res.json())
-      .then(json => setProducts(json));
+      .then(json => {
+        setLoading(false);
+        setProducts(json);
+      });
   }, []);
 
   return (
@@ -20,15 +33,28 @@ function App() {
           <Heading as="h3" size="2xl" color="teal.400">
             BIG SHOP!
           </Heading>
-          <Grid templateColumns="repeat(4, 1fr)" gap={6} mt={20}>
-            {products.map(({ id, title, image, price }) => (
-              <ProductCard
-                key={id}
-                {...{ title, image, price }}
-                isNew={!(id % 3)}
-              />
-            ))}
-          </Grid>
+
+          {loading && (
+            <Spinner
+              thickness="4px"
+              speed="0.65s"
+              emptyColor="gray.200"
+              color="blue.500"
+              size="xl"
+            />
+          )}
+
+          {!loading && (
+            <Grid templateColumns="repeat(4, 1fr)" gap={6} mt={20}>
+              {products.map(({ id, title, image, price }) => (
+                <ProductCard
+                  key={id}
+                  {...{ title, image, price }}
+                  isNew={!(id % 3)}
+                />
+              ))}
+            </Grid>
+          )}
         </Grid>
       </Box>
     </ChakraProvider>
